953a283932b2d09993a0a7073d2075ad0e2b10ed
talkz199 <tal.katz@runi.post.ac.il>
2025-11-07 13:26:53 +0200

    final bill

--- Bill3.java
+++ Bill3.java
@@ -5,10 +5,9 @@ public class Bill3 {
         String name1 = args[0];
         String name2 = args[1];
         String name3 = args[2];
-        
-        int  bill = Double.parseDouble(args[3]);
+        int  bill = Integer.parseInt(args[3]);
         bill = bill / 3;
-       System.out.println("Dear " + name1 + ", " + name2 + "and " + name3 + "pay " + bill + " Shekels each.");
+       System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + bill + " Shekels each.");
     }
     
 }
