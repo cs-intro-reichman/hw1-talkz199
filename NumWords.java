9b6ab28ac025314ed3daf5ba053a26a5c09d8ee4
talkz199 <tal.katz@runi.post.ac.il>
2025-11-07 14:41:14 +0200

    NumWords finito

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,10 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		 int hundreds = num / 100;
+		 int tens = (num - hundreds * 100) / 10;
+		 int ones = (num- hundreds * 100 - tens * 10);
+		 System.out.println(hundreds +" hundreds, " + tens + " tens, and " + ones +" ones.");
 	}
 }
