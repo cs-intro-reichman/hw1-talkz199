aa79fd52ae3b7bf2d43f706f57405106f8f22935
talkz199 <tal.katz@runi.post.ac.il>
2025-11-07 17:27:11 +0200

    time format finito++ last

--- TimeFormat.java
+++ TimeFormat.java
@@ -16,19 +16,19 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         // Replace this comment with the rest of your code
-		String status = "AM" ;
+		String status = " AM" ;
 	
 		if( hours > 12 )
 		{
 			 hours = hours - 12;
-			 status = "PM";
+			 status = " PM";
 			System.out.print(hours);
 		
 
 
 		} else { System.out.print(hours);}
 		System.out.print(":");
-		
+
 		if (minutes < 10) {
 			System.out.print("0" + minutes);
 		} else {System.out.print(minutes);}
